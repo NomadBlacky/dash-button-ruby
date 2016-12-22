c9397eb3a2c0b4255ded408b2974e1a49580c04d
NomadBlacky <hourou.hamurinzu@gmail.com>
2016-12-23 02:46:21 +0900

    Add a scratch of packet capture

--- capture.rb
+++ capture.rb
@@ -1,25 +1,30 @@
 #!/usr/bin/env ruby
 
+require 'csv'
 require "packetfu"
 include PacketFu
 
-def get_capture(iface,filter)
-  cap = Capture.new(:iface=>iface, :start=>true, :filter=>filter, :timeout=>5000)
-  cap.stream.each do |pkt|
-    next unless ARPPacket.can_parse?(pkt)
-    t_stamp  = Time.now.strftime("%Y-%m-%d %H:%M:%S.%6N")
-    arp_packet = ARPPacket.parse(pkt)
-    src_mac = EthHeader.str2mac(arp_packet.eth_src)
-    #src_ip = IPHeader.octet_array(arp_packet.ip_src).join('.')
-    #src_port = arp_packet.tcp_src
-    puts "#{t_stamp} src_mac: #{src_mac}"
+def get_capture(filter)
+  ouis = Hash[CSV.read('lib/oui.csv').map{|ary| [ary[1], ary[2]]}]
+  ouis.default = "unknown"
+
+  capture = Capture.new(iface: Utils.whoami?[:iface], start: true, filter: filter, timeout: 5000)
+  puts "Start capture."
+  capture.stream.each do |packet|
+    if ARPPacket.can_parse?(packet)
+      time_stamp = Time.now.strftime("%Y-%m-%d %H:%M:%S.%6N")
+      arp_packet = ARPPacket.parse(packet)
+      src_mac = EthHeader.str2mac(arp_packet.eth_src)
+      manufacturer = ouis[src_mac.gsub(':', '')[0, 6].upcase]
+      #binding.pry
+      puts "#{time_stamp} => #{src_mac} [#{manufacturer}]"
+    end
   end
 
 end
 
 
 if $0 == __FILE__
-  iface = ARGV[0]
-  filter = ARGV[1]
-  get_capture(iface, filter)
+  filter = ARGV[0]
+  get_capture(filter)
 end
